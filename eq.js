9c3f78c77c5c4e0708d8541d97d618887179839e
Liam Kishawi <lkishawi3@protonmail.ch>
2023-03-14 14:16:33 +1000

    1.6

--- eq.js
+++ eq.js
@@ -18,7 +18,7 @@ function playAudio() {
   } else {
     audio.src = "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3";
   }
-  '');
+
 
   audio.load();
   audio.play()
